61d92dc01ec8373eb3e8d5458094d25ed6a4e257
Shravang007 <shravangoudgajula@gmail.com>
2023-07-05 09:01:54 +0530

    docdb

--- main.tf
+++ main.tf
@@ -5,4 +5,13 @@ resource "aws_docdb_subnet_group" "main" {
   tags = merge({ Name = "${var.component}-${var.env}" }, var.tags)
 }
 
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier      = "my-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
 
